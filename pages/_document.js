8efffd0a04947e3427fc98eff4f12fb9b3c3f915
willibu2 <willibu2@hotmail.com>
2023-04-19 13:38:36 +0200

    removed title

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <title>Age Calculator App</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
